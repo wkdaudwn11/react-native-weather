d2593a35069bfdb33f89651a461274a28febe4d1
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-09-19 11:06:28 +0900

    위치 정보 접근 권한 물어본 후, 날씨 정보 가져오기

--- App.js
+++ App.js
@@ -1,16 +1,43 @@
 import React, { Component } from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import axios from "axios";
+
+const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
 
 export default class extends Component {
+  state = {
+    isLoading: true,
+  };
+
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log("data >", data);
+  };
+
   getLocation = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert(
+        "위치를 불러올 수가 없습니다.",
+        "위치 관련 접근 권한을 확인해주세요."
+      );
+    }
   };
   componentDidMount() {
     this.getLocation();
   }
   render() {
-    return <Loading />;
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
